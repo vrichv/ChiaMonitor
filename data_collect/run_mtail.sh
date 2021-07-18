ce67213fc05d83f9a83abe4f2e47f45c8c975f5e
R <1572349+vrichv@users.noreply.github.com>
2021-07-18 13:53:01 +0800

    extract env

--- data_collect/run_mtail.sh
+++ data_collect/run_mtail.sh
@@ -1,4 +1,8 @@
 # ~/.chia/mainnet/log/ is where your Chia logs saved.
 # Replace them if they are different on your host
-nohup watch -n 30 "../venv/bin/chia farm summary > farmer_summary.log" &
-nohup ./mtail --progs ./ --logs ~/.chia/mainnet/log/debug.log --logs ./farmer_summary.log --poll_interval 3000ms &
+CHIA_BIN=chia
+MTAIL_BIN=mtail
+MTAIL_CONF=~/bin/mtail/
+LOG_FILE=/tmp/farmer_summary.log
+nohup watch -n 30 "$CHIA_BIN farm summary > $LOG_FILE" &
+nohup $MTAIL_BIN --progs $MTAIL_CONF --logs ~/.chia/mainnet/log/debug.log --logs $LOG_FILE --poll_interval 3000ms &
